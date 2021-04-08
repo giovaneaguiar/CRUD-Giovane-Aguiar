d4f5bae719ace83562eaa18afd8e52f7fd57d940
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-07 21:00:16 -0300

    Cria component para ler os produtos, "read".

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +28,8 @@ import { MatInputModule } from '@angular/material/input';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
